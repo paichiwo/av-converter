b2ab0024030b975c539dfba84b81cde1db06fd35
paichiwo <lzerucha@gmail.com>
2024-06-23 21:50:04 +0100

    Add output_formats

--- src/config.py
+++ src/config.py
@@ -5,16 +5,35 @@ VERSION = '1.2.0'
 IMG_PATHS = {
     'icon': resource_path('images/audio-video_converter_icon_512x512.ico'),
     'icon_png': resource_path('images/audio-video_converter_icon_64x64.png'),
-    'bg': resource_path('images/background.png'),
-    'info': resource_path('images/info_icon.png'),
     'settings': resource_path('images/settings_icon.png'),
-    'browse': resource_path('images/browse_button.png'),
-    'convert': resource_path('images/convert_button.png'),
-    'clear': resource_path('images/clear_button.png'),
     'filepath': resource_path('images/filepath.png'),
     'folder': resource_path('images/folder_icon.png'),
     'plus': resource_path('images/plus_16x16.png'),
     'plus_large': resource_path('images/plus.png'),
-    'save': resource_path('images/save_button.png'),
     'github': resource_path('images/github_icon_32x32.png')
 }
+
+OUTPUT_FORMATS = [
+    '.webm',
+    '.mpg',
+    '.mp2',
+    '.mpeg',
+    '.mpe',
+    '.mpv',
+    '.ogg',
+    '.mp4',
+    '.m4p',
+    '.m4v',
+    '.avi',
+    '.wmv',
+    '.mov',
+    '.qt',
+    '.flv',
+    '.swf',
+    '.mp3',
+    '.flac',
+    '.wav',
+    '.aac',
+    '.aiff',
+    '.m4a'
+]
