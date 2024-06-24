1f612606ea8c1987b2acc556446cdc76e94f5ae3
paichiwo <lzerucha@gmail.com>
2024-06-24 21:40:47 +0100

    Remove redundant code

--- src/helpers.py
+++ src/helpers.py
@@ -2,8 +2,6 @@ import json
 from os import environ, path, makedirs, startfile
 from customtkinter import CTkImage
 from PIL import Image
-from src.resource_path import resource_path
-from zipfile import ZipFile
 
 
 def center_window(window, width, height):
@@ -31,7 +29,7 @@ def imager(file_path, x, y):
 
 def load_settings():
     """Load settings from JSON file"""
-    file_path = path.join(environ['LOCALAPPDATA'], 'Tube-Getter', 'settings.json')
+    file_path = path.join(environ['LOCALAPPDATA'], 'AV-Converter', 'settings.json')
 
     try:
         with open(file_path, 'r') as file:
@@ -43,7 +41,7 @@ def load_settings():
 
 def save_settings(data, file_name):
     """Save settings to JSON file"""
-    file_path = path.join(environ['LOCALAPPDATA'], 'Tube-Getter')
+    file_path = path.join(environ['LOCALAPPDATA'], 'AV-Converter')
     settings_path = path.join(file_path, file_name)
 
     if not path.exists(file_path):
@@ -56,15 +54,3 @@ def save_settings(data, file_name):
 def open_downloads_folder():
     """Open target folder"""
     startfile(load_settings())
-
-
-def unzip_ffmpeg():
-    """Unzip ffmpeg file"""
-    dir_path = path.join(environ['LOCALAPPDATA'], 'Tube-Getter')
-    exe_path = path.join(dir_path, 'ffmpeg.exe')
-
-    if not path.exists(exe_path):
-        makedirs(dir_path, exist_ok=True)
-
-    with ZipFile(resource_path('ffmpeg/ffmpeg.zip'), 'r') as zip_ref:
-        zip_ref.extract('ffmpeg.exe', dir_path)
