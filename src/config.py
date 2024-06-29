ae29313dacfd5285a831624f57e4a4d2777bd93d
paichiwo <lzerucha@gmail.com>
2024-06-29 15:17:40 +0100

    Remove unused images

--- src/config.py
+++ src/config.py
@@ -6,9 +6,6 @@ IMG_PATHS = {
     'icon': resource_path('images/audio-video_converter_icon_512x512.ico'),
     'icon_png': resource_path('images/audio-video_converter_icon_64x64.png'),
     'settings': resource_path('images/settings_icon.png'),
-    'filepath': resource_path('images/filepath.png'),
-    'folder': resource_path('images/folder_icon.png'),
-    'plus': resource_path('images/plus_16x16.png'),
     'plus_large': resource_path('images/plus.png'),
     'github': resource_path('images/github_icon_32x32.png')
 }
